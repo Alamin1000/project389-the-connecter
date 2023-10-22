eebd52df2f80ccd86e4b9f2a57433aeb92ec5df9
alamin <alaminhosen44000@gmail.com>
2023-10-22 12:45:04 +0600

    1.10

--- js/main.js
+++ js/main.js
@@ -1,5 +1,31 @@
 (function ($) {
   "use strict";
+
+  //magnificPopup
+  $(".popup-img").magnificPopup({
+    type: "image",
+  });
+  $(".popup-video").magnificPopup({
+    type: "iframe",
+  });
+  // magnific-popup-img-gallery
+  $(".album-img a").magnificPopup({
+    type: "image",
+    gallery: {
+      enabled: true,
+    },
+  });
+  //multiple-img-gallery
+  $(".image-box").each(function () {
+    // the containers for all your galleries
+    $(this).magnificPopup({
+      delegate: ".img", // the selector for gallery item
+      type: "image",
+      gallery: {
+        enabled: true,
+      },
+    });
+  });
 })(jQuery);
 
 $(document).ready(function () {});
